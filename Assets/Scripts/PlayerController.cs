ee1dc6b93c4e79bf454a0ba3716f1d93c46f0f60
Ilya Melnikov <ilya.mel2018@mail.ru>
2025-11-07 18:03:43 +0300

    add cloud controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private FreeCamera m_camera;
+    [SerializeField] private GameObject m_uiPanel;
+    [SerializeField] private CloudController m_cloudController;
+    
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (m_uiPanel.activeSelf) return;
+        m_camera.Move();
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            m_cloudController.MoveNext();
+        }
+    }
+}
