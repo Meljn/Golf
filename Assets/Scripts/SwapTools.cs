aa2e23c88de8fa1a7e8a7c63d86b78648aaa53fb
Meljn <ilya.mel2018@mail.ru>
2025-11-10 22:51:00 +0300

    some changes

--- Assets/Scripts/SwapTools.cs
+++ Assets/Scripts/SwapTools.cs
@@ -3,29 +3,29 @@ using UnityEngine;
 
 public class SwapTools : MonoBehaviour
 {
-    [SerializeField] private Transform[] tools;
+    [SerializeField] private Transform[] m_tools;
 
     public void Swap()
     {
-        if (tools.Length < 2) return;
+        if (m_tools.Length < 2) return;
 
-        Transform[] parents = new Transform[tools.Length];
-        for (int i = 0; i < tools.Length; i++)
-            parents[i] = tools[i].parent;
+        Transform[] parents = new Transform[m_tools.Length];
+        for (int i = 0; i < m_tools.Length; i++)
+            parents[i] = m_tools[i].parent;
 
-        for (int i = 0; i < tools.Length; i++)
+        for (int i = 0; i < m_tools.Length; i++)
         {
-            int r = Random.Range(i, tools.Length);
-            Transform temp = tools[i];
-            tools[i] = tools[r];
-            tools[r] = temp;
+            int r = Random.Range(i, m_tools.Length);
+            Transform temp = m_tools[i];
+            m_tools[i] = m_tools[r];
+            m_tools[r] = temp;
         }
 
-        for (int i = 0; i < tools.Length; i++)
+        for (int i = 0; i < m_tools.Length; i++)
         {
-            tools[i].SetParent(parents[i]);
-            tools[i].localPosition = Vector3.zero;
-            tools[i].localRotation = Quaternion.identity;
+            m_tools[i].SetParent(parents[i]);
+            m_tools[i].localPosition = Vector3.zero;
+            m_tools[i].localRotation = Quaternion.identity;
         }
     }
 }
