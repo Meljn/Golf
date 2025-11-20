2823073af1e7115307ec7657c7c914b98ef52c05
Meljn <ilya.mel2018@mail.ru>
2025-11-20 21:03:44 +0300

    add new scene and fix bigs

--- Assets/Scripts/Stick.cs
+++ Assets/Scripts/Stick.cs
@@ -1,54 +1,54 @@
-using System;
 using UnityEngine;
 
 namespace Golf
 {
     public class Stick : MonoBehaviour
     {
-        [SerializeField] [Min(0)] private float m_power = 250;
+        [SerializeField][Min(0)] private float m_power = 250;
         [SerializeField] private Transform m_point;
-        [SerializeField] private float m_minAngelZ = -30;
-        [SerializeField] private float m_maxAngelZ = 30;
-        [SerializeField] [Min(0)] private float m_speed;
+        [SerializeField] private float m_minAngleZ = -30;
+        [SerializeField] private float m_maxAngleZ = 30;
+        [SerializeField][Min(0)] private float m_speed;
+        [SerializeField] private GameObject m_particle;
 
-        private bool m_isDown;
         private Vector3 m_direction;
         private Vector3 m_lastPointPosition;
-
+        private bool m_isDown;
         private void FixedUpdate()
         {
-            var angels = transform.localEulerAngles;
-            
+            var angles = transform.localEulerAngles;
             if (m_isDown)
             {
-                angels.z = Rotate(angels.z, m_minAngelZ);
+                angles.z = Rotate(angles.z, m_minAngleZ);
             }
             else
             {
-                angels.z = Rotate(angels.z, m_maxAngelZ);
+                angles.z = Rotate(angles.z, m_maxAngleZ);
             }
 
-            transform.localEulerAngles = angels;
+            transform.localEulerAngles = angles;
+
             m_direction = (m_point.position - m_lastPointPosition).normalized;
-            m_lastPointPosition = transform.position;         
+            m_lastPointPosition = m_point.position;
         }
-        
+        public void Down() => m_isDown = true;
+        public void Up() => m_isDown = false;
+
         private float Rotate(float angleZ, float target)
         {
             return Mathf.MoveTowardsAngle(angleZ, target, Time.deltaTime * m_speed);
         }
-        
-        private void OnCollisionEnter(Collision collision)
-        {  
-            if (collision.gameObject.TryGetComponent<StoneComponent>(out var stoneComponent))
-            {
-                stoneComponent.AddForce(m_power * m_direction);
-            }
-            
 
+        private void OnCollisionEnter(Collision collision)
+        {
+
+            if (collision.gameObject.TryGetComponent<StoneComponent>(out var stone))
+            {
+                stone.AddForce(m_power * m_direction);
+                Instantiate(m_particle, stone.transform.position, stone.transform.rotation);
+            }
         }
 
-        public void Down() => m_isDown = true;
-        public void Up() => m_isDown = false;
+
     }
 }
--- Assets/Scripts/StoneSpawner.cs
+++ Assets/Scripts/StoneSpawner.cs
@@ -6,11 +6,15 @@ namespace Golf
     {
         [SerializeField] private StoneComponent[] m_prefabs;
         [SerializeField] private Transform m_spawnPoint;
+        [SerializeField] private float m_spawnRadius = 0.5f;
 
         public StoneComponent Spawn()
         {
             var prefab = m_prefabs[Random.Range(0, m_prefabs.Length)];
-            return Instantiate(prefab, m_spawnPoint.position, m_spawnPoint.rotation);
+            Vector3 randomOffset = Random.insideUnitSphere * m_spawnRadius;
+            Vector3 spawnPosition = m_spawnPoint.position + randomOffset;
+
+            return Instantiate(prefab, spawnPosition, m_spawnPoint.rotation);
         }
     } 
 }
